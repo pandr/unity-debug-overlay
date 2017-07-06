ae4175880efb656f4f14bd3166934967ceb73a71
Peter Andreasen <pandreasen@gmail.com>
2017-07-06 15:11:41 +0200

    simplified stuff by getting rid of char buffer overlay. Now everything is just quads

--- Assets/DebugOverlay/DebugOverlay.cs
+++ Assets/DebugOverlay/DebugOverlay.cs
@@ -24,69 +24,41 @@ public class DebugOverlay : MonoBehaviour
     public void Init()
     {
         instance = this;
-
-        Resize(width, height);
     }
 
-    public static void Write(int x, int y, string format)
+    public static void Write(float x, float y, string format)
     {
         instance._Write(x, y, format, new ArgList0());
     }
     public static void Write(string format)
     {
-        int x, y;
-        instance.RelPos(Position.Below, out x, out y);
-        instance._Write(x, y, format, new ArgList0());
+        instance._Write(format, new ArgList0());
     }
 
-    public static void Write<T>(int x, int y, string format, T arg)
+    public static void Write<T>(float x, float y, string format, T arg)
     {
         instance._Write(x, y, format, new ArgList1<T>(arg));
     }
 
     public static void Write<T>(string format, T arg)
     {
-        int x, y;
-        instance.RelPos(Position.Below, out x, out y);
-        instance._Write(x, y, format, new ArgList1<T>(arg));
+        instance._Write(format, new ArgList1<T>(arg));
     }
-    public static void Write<T0, T1>(int x, int y, string format, T0 arg0, T1 arg1)
+    public static void Write<T0, T1>(float x, float y, string format, T0 arg0, T1 arg1)
     {
         instance._Write(x, y, format, new ArgList2<T0, T1>(arg0, arg1));
     }
     public static void Write<T0, T1>(string format, T0 arg0, T1 arg1)
     {
-        int x, y;
-        instance.RelPos(Position.Below, out x, out y);
-        instance._Write(x, y, format, new ArgList2<T0, T1>(arg0, arg1));
+        instance._Write(format, new ArgList2<T0, T1>(arg0, arg1));
     }
-    public static void Write<T0, T1, T2>(int x, int y, string format, T0 arg0, T1 arg1, T2 arg2)
+    public static void Write<T0, T1, T2>(float x, float y, string format, T0 arg0, T1 arg1, T2 arg2)
     {
         instance._Write(x, y, format, new ArgList3<T0, T1, T2>(arg0, arg1, arg2));
     }
     public static void Write<T0, T1, T2>(string format, T0 arg0, T1 arg1, T2 arg2)
     {
-        int x, y;
-        instance.RelPos(Position.Below, out x, out y);
-        instance._Write(x, y, format, new ArgList3<T0, T1, T2>(arg0, arg1, arg2));
-    }
-
-    public enum Position
-    {
-        Below,
-        Above,
-        Right,
-    }
-
-    void RelPos(Position pos, out int x, out int y)
-    {
-        switch (pos)
-        {
-            default:
-            case Position.Below: x = m_LastWriteX; y = m_LastWriteY + 1; return;
-            case Position.Above: x = m_LastWriteX; y = m_LastWriteY - 1; return;
-            case Position.Right: x = m_LastEndX; y = m_LastEndY; return;
-        }
+        instance._Write(format, new ArgList3<T0, T1, T2>(arg0, arg1, arg2));
     }
 
     // Draw a stacked histogram from numSets of data. Data must contain numSets of interleaved, non-negative datapoints.
@@ -107,6 +79,19 @@ public class DebugOverlay : MonoBehaviour
         instance._DrawRect(x, y, w, h, col);
     }
 
+    void _DrawText(float x, float y, ref char[] text, int length, Color color)
+    {
+        var idx = AllocQuads(length);
+        QuadData qd;
+        qd.color = color;
+        for(var i = 0; i < length; i++)
+        {
+            qd.character = text[i];
+            qd.rect = new Vector4(x + i, y, 1, 1);
+            m_QuadDatas[idx + i] = qd;
+        }        
+    }
+
     void _DrawHist(float x, float y, float w, float h, float[] data, Color[] color, int numSets, float maxRange = -1.0f)
     {
         if (data.Length % numSets != 0)
@@ -114,8 +99,9 @@ public class DebugOverlay : MonoBehaviour
         if (color.Length != numSets)
             throw new System.ArgumentException("Length of colors must be numSets");
 
-        var numSamples = data.Length / numSets;
-        var idx = AllocExtras(numSets * numSamples);
+        var dataLength = data.Length;
+        var numSamples = dataLength / numSets;
+        var idx = AllocQuads(numSets * numSamples);
 
         float maxData = float.MinValue;
 
@@ -136,72 +122,77 @@ public class DebugOverlay : MonoBehaviour
         float dx = w / numSamples;
         float scale = maxRange > minRange ? h / (maxRange - minRange) : 1.0f;
 
-        ExtraData extraData;
-        extraData.character = '\0';
-        for (var i = 0; i < numSamples; i++)
+        QuadData qd;
+        qd.character = '\0';
+        float stackOffset = 0;
+        for (var i = 0; i < dataLength; i++)
         {
-            float stackOffset = 0;
-            for (var j = 0; j < numSets; j++)
-            {
-                extraData.color = color[j];
-                float d = data[i * numSets + j];
-                float scaledData = d * scale; // now in [0, h]
-                extraData.rect = new Vector4(x + dx * i, y + h - d * scale - stackOffset, dx, d * scale);
-                stackOffset += scaledData;
-                m_Extras[idx + i * numSets + j] = extraData;
-            }
+            var set = i % numSets;
+            if(set == 0)
+                stackOffset = 0;
+            var c = color[set];
+            qd.color.x = c.r;
+            qd.color.y = c.g;
+            qd.color.z = c.b;
+            qd.color.w = c.a;
+            float d = data[i];
+            float scaledData = d * scale; // now in [0, h]
+            qd.rect.x = x + dx * i;
+            qd.rect.y = y + h - d * scale - stackOffset;
+            qd.rect.z = dx;
+            qd.rect.w = d * scale;
+            stackOffset += scaledData;
+            m_QuadDatas[idx++] = qd;
         }
     }
 
     void _DrawRect(float x, float y, float w, float h, Color col)
     {
-        ExtraData rd;
+        QuadData rd;
         rd.rect = new Vector4(x, y, w, h);
         rd.color = col;
-        var idx = AllocExtras(1);
+        var idx = AllocQuads(1);
         rd.character = '\0';
-        m_Extras[idx] = rd;
-    }
-
-    void Resize(int width, int height)
-    {
-        m_CharBuffer = new char[width * height];
+        m_QuadDatas[idx] = rd;
     }
 
     void _Clear()
     {
-        for (int i = 0, c = m_CharBuffer.Length; i < c; i++)
-            m_CharBuffer[i] = (char)0;
-        m_NumExtrasUsed = 0;
+        m_NumQuadsUsed = 0;
     }
 
-    void _Write<T>(int x, int y, string format, T argList) where T : IArgList
+    void _Write<T>(string format, T argList) where T : IArgList
     {
-        int i = width * y + x;
-        if (i < 0 || i >= m_CharBuffer.Length)
-            return;
-        var num = StringFormatter.__Write<T>(ref m_CharBuffer, i, format, argList);
-        Wrote(x, y, num);
+        _Write(m_LastWriteX, m_LastWriteY + 1, format, argList);
     }
 
-    int AllocExtras(int num)
+    char[] _buf = new char[1024];
+    void _Write<T>(float x, float y, string format, T argList) where T : IArgList
     {
-        var idx = m_NumExtrasUsed;
-        m_NumExtrasUsed += num;
-        if (m_NumExtrasUsed > m_Extras.Length)
+        var num = StringFormatter.__Write<T>(ref _buf, 0, format, argList);
+        _DrawText(x, y, ref _buf, num, m_CurrentColor);
+        m_LastWriteX = x;
+        m_LastWriteY = y;
+    }
+
+    int AllocQuads(int num)
+    {
+        var idx = m_NumQuadsUsed;
+        m_NumQuadsUsed += num;
+        if (m_NumQuadsUsed > m_QuadDatas.Length)
         {
-            var newRects = new ExtraData[m_NumExtrasUsed + 128];
-            System.Array.Copy(m_Extras, newRects, m_Extras.Length);
-            m_Extras = newRects;
+            var newRects = new QuadData[m_NumQuadsUsed + 128];
+            System.Array.Copy(m_QuadDatas, newRects, m_QuadDatas.Length);
+            m_QuadDatas = newRects;
         }
         return idx;
     }
 
     public void Tick()
     {
-        // Resize or recreate buffer if needed. Not we use extras.Length which is high-water mark for extras
+        // Resize or recreate buffer if needed. Not we use m_QuadDatas.Length which is high-water mark for quads
         // to avoid constant recreation.
-        var requiredInstanceCount = width * height + m_Extras.Length;
+        var requiredInstanceCount = m_QuadDatas.Length;
         if (m_InstanceBuffer == null || requiredInstanceCount != m_QuadCount)
         {
             if (m_InstanceBuffer != null)
@@ -219,62 +210,37 @@ public class DebugOverlay : MonoBehaviour
                 (float)cellHeight / instanceMaterialProc.mainTexture.height));
         }
 
-        // Scan out content of screen buffer
-        m_usedQuads = 0;
-        int screenWidth = width;
-        var v4 = new Vector4();
-        var size = new Vector4(1, 1, 0, 0);
-        var color = new Color(2, 1.0f, 0, 0.0f);
-        for (int i = 0, c = m_CharBuffer.Length; i < c; i++)
+        // Scan out quads
+        for (int i = 0, c = m_NumQuadsUsed; i < c; i++)
         {
-            var ch = m_CharBuffer[i];
-            if (ch == 0)
-                continue;
-            v4.x = i % screenWidth; // pos
-            v4.y = i / screenWidth; // pos
-            v4.z = (ch - 32) % charCols; // uv
-            v4.w = (ch - 32) / charCols; // uv
-            m_DataBuffer[m_usedQuads].positionAndUV = v4;
-            m_DataBuffer[m_usedQuads].size = size;
-            m_DataBuffer[m_usedQuads].color = color;
-            m_usedQuads++;
-        }
-
-        // Scan out extras
-        for (int i = 0, c = m_NumExtrasUsed; i < c; i++)
-        {
-            var r = m_Extras[i];
+            var r = m_QuadDatas[i];
+            var col = r.color;
             var posUV = new Vector4(r.rect.x, r.rect.y, 0, 0);
             if (r.character != '\0')
             {
                 posUV.z = (r.character - 32) % charCols;
                 posUV.w = (r.character - 32) / charCols;
+                col.w = 0.0f;
             }
-            m_DataBuffer[m_usedQuads].positionAndUV = posUV;
-            m_DataBuffer[m_usedQuads].size = new Vector4(r.rect.z, r.rect.w, 0, 0);
-            m_DataBuffer[m_usedQuads].color = r.color;
-            m_usedQuads++;
+            m_DataBuffer[i].positionAndUV = posUV;
+            m_DataBuffer[i].size = new Vector4(r.rect.z, r.rect.w, 0, 0);
+            m_DataBuffer[i].color = col;
         }
-        m_InstanceBuffer.SetData(m_DataBuffer, 0, 0, m_usedQuads);
+        m_InstanceBuffer.SetData(m_DataBuffer, 0, 0, m_NumQuadsUsed);
     }
 
     void OnPostRender()
     {
         instanceMaterialProc.SetPass(0);
-        Graphics.DrawProcedural(MeshTopology.Triangles, m_usedQuads * 6, 1);
-
-        if (width * height != m_CharBuffer.Length)
-            Resize(width, height);
+        Graphics.DrawProcedural(MeshTopology.Triangles, m_NumQuadsUsed * 6, 1);
 
         _Clear();
     }
 
-    void Wrote(int x, int y, int num)
+    void Wrote(float x, float y)
     {
         m_LastWriteX = x;
         m_LastWriteY = y;
-        m_LastEndX = (x + num) % width;
-        m_LastEndY = (x + y * width + num) / width;
     }
 
     public void DeInit()
@@ -286,13 +252,11 @@ public class DebugOverlay : MonoBehaviour
         m_DataBuffer = null;
     }
 
-    char[] m_CharBuffer;
-    int m_LastWriteX;
-    int m_LastWriteY;
-    int m_LastEndX;
-    int m_LastEndY;
+    float m_LastWriteX;
+    float m_LastWriteY;
+    Color m_CurrentColor = Color.grey;
 
-    struct ExtraData
+    struct QuadData
     {
         public Vector4 rect;
         public Vector4 color;
@@ -306,13 +270,12 @@ public class DebugOverlay : MonoBehaviour
         public Vector4 color;
     }
 
-    int m_NumExtrasUsed = 0;
-    ExtraData[] m_Extras = new ExtraData[0];
+    int m_NumQuadsUsed = 0;
+    QuadData[] m_QuadDatas = new QuadData[0];
 
     int m_QuadCount = -1;
 
     ComputeBuffer m_InstanceBuffer;
     InstanceData[] m_DataBuffer;
-    int m_usedQuads = 0;
 
 }
