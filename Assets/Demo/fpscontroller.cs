18e88f2f6f396b32f21b5ded414f25c621a4400e
Peter Andreasen <pandreasen@gmail.com>
2017-12-27 16:11:42 +0100

    tweaked movement

--- Assets/Demo/fpscontroller.cs
+++ Assets/Demo/fpscontroller.cs
@@ -16,13 +16,18 @@ public class fpscontroller : MonoBehaviour
 {
     public Camera player_cam;
 
-    public float mouse_sensitivity = 50.0f;
-    public float player_speed = 5.0f;
+    public float mouse_sensitivity = 75.0f;
     public float jump_speed = 5.0f;
+    public float player_friction = 8.0f;
+    public float player_air_friction = 1.0f;
+    public float player_accel = 100.0f;
+    public float player_air_accel = 30.0f;
+    public float player_speed = 7.0f;
 
-    private float vertical_speed = 0.0f;
+    private Vector3 velocity = Vector3.zero;
 
     CharacterController cc;
+
     void Start()
     {
         cc = GetComponent<CharacterController>();
@@ -30,33 +35,66 @@ public class fpscontroller : MonoBehaviour
 
     void Update()
     {
-        // Move around with WASD
-        var move = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        move = move * player_speed * Time.deltaTime;
-        move = transform.TransformDirection(move);
-        cc.Move(move);
-
-        // Fall down / gravity
-        vertical_speed = vertical_speed - 10.0f * Time.deltaTime;
-        if (vertical_speed < -10.0f)
-            vertical_speed = -10.0f; // max fall speed
-        var vertical_move = new Vector3(0, vertical_speed * Time.deltaTime, 0);
-        cc.Move(vertical_move);
-
         // Turn player
         var turn_player = new Vector3(0, Input.GetAxisRaw("Mouse X"), 0);
         turn_player = turn_player * mouse_sensitivity * Time.deltaTime;
         transform.localEulerAngles += turn_player;
 
+        // Fall down / gravity
+        velocity.y = velocity.y - 10.0f * Time.deltaTime;
+        if (velocity.y < -10.0f)
+            velocity.y = -10.0f; // max fall speed
+        var vertical_move = new Vector3(0, velocity.y * Time.deltaTime, 0);
+        cc.Move(vertical_move);
+
+        bool isGrounded = cc.isGrounded;
+
+        var friction = isGrounded ? player_friction : player_air_friction;
+        var accel = isGrounded ? player_accel : player_air_accel;
+
+        // WASD movement
+        var move = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        var moveMagnitude = move.magnitude;
+        if (moveMagnitude > 1.0f)
+            move /= moveMagnitude;
+        // Transform to world space
+        move = transform.TransformDirection(move);
+
+        // Apply friction
+        var groundVelocity = new Vector3(velocity.x, 0, velocity.z);
+        float g_speed = groundVelocity.magnitude;
+        if (g_speed > 0)
+        {
+            g_speed -= Mathf.Max(g_speed, 1.0f) * friction * Time.deltaTime;
+            if (g_speed < 0)
+                g_speed = 0;
+            groundVelocity = groundVelocity.normalized * g_speed;
+        }
+
+        // Horizontal movement
+        var wantedGroundVel = move * player_speed;
+        var wantedGroundDir = moveMagnitude > 0.001f ? move / moveMagnitude : Vector3.zero;
+        var speedMadeGood = Vector3.Dot(groundVelocity, wantedGroundDir);
+        var deltaSpeed = moveMagnitude * player_speed - speedMadeGood;
+        if (deltaSpeed > 0.001)
+        {
+            var velAdjust = Mathf.Clamp(accel * Time.deltaTime, 0.0f, deltaSpeed) * wantedGroundDir;
+            groundVelocity += velAdjust;
+        }
+
+        velocity.x = groundVelocity.x;
+        velocity.z = groundVelocity.z;
+        cc.Move(velocity * Time.deltaTime);
+
+        // Jump
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            velocity.y = jump_speed;
+        }
+
         // Camera look up/down
         var turn_cam = new Vector3(-Input.GetAxisRaw("Mouse Y"), 0, 0);
         turn_cam = turn_cam * mouse_sensitivity * Time.deltaTime;
         player_cam.transform.localEulerAngles += turn_cam;
-
-        // Jump
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            vertical_speed = jump_speed;
-        }
     }
 }
